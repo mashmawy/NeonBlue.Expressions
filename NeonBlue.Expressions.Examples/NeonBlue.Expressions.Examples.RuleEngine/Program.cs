d6c8b8b77cdf366d29a986583488a1b060369426
Mohamed Hassan ashmawy <ashmawyonline@gmail.com>
2024-09-24 00:15:38 +0300

    Add Rule Engine Example Project

---
+++ NeonBlue.Expressions.Examples/NeonBlue.Expressions.Examples.RuleEngine/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using NeonBlue.Expressions;
+using NeonBlue.Expressions.Examples.RuleEngine;
+
+var rule1 = new Rule("Rule 1", "x > 10");
+var rule2 = new Rule("Rule 2", "y < 5");
+var rule3 = new Rule("Rule 3", "z == 0");
+
+rule1.AddChild(rule2);
+rule1.AddChild(rule3);
+
+var parameters = new ExpressionParameters(
+    new ExpressionParameter("x", 12),
+    new ExpressionParameter("y", 3),
+    new ExpressionParameter("z", 0)
+);
+
+var result = rule1.Evaluate(parameters);
+
+Console.WriteLine(result.ToString());
---
+++ NeonBlue.Expressions.Examples/NeonBlue.Expressions.Examples.RuleEngine/Rule.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information 
+namespace NeonBlue.Expressions.Examples.RuleEngine
+{
+    public class Rule
+    {
+        public string Name { get; set; }
+        public string Expression { get; set; }
+        public List<Rule> Children { get; set; }
+
+        public Rule(string name, string expression)
+        {
+            Name = name;
+            Expression = expression;
+            Children = [];
+        }
+
+        public void AddChild(Rule child)
+        {
+            Children.Add(child);
+        }
+
+        public bool Evaluate(ExpressionParameters parameters)
+        {
+            // Evaluate the expression using your preferred expression evaluator
+            var result = EvaluateExpression(Expression, parameters);
+
+            // If the rule has children, evaluate them recursively
+            if (Children.Any())
+            {
+                foreach (var child in Children)
+                {
+                    if (!child.Evaluate(parameters))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static bool EvaluateExpression(string expression, ExpressionParameters parameters)
+        {
+            // Implement your expression evaluation logic here
+            // For example, you could use the NeonBlue.Expressions library
+            var evaluator = new Evaluator();
+            return evaluator.Evaluate<bool>(expression, parameters);
+        }
+    }
+}
